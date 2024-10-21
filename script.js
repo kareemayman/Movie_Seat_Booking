d82d4e9e45b80e9321dccd1459a89c35e4982f16
Kareemayman <Kareem.Mohamed.ayman@gmail.com>
2024-10-21 18:59:46 +0300

    Added Selected Seats Counter

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
-const seats = document.querySelector('.seats')
+const seats = document.querySelector(".seats")
+const selectedSeatsCount = document.querySelector(".selection .num-of-seats")
 
-seats.addEventListener('click', e => {
+let seatsArr = [] // 0 => empty, 1 => selected, 2 => occupied
 
-    if (e.target.matches('.seat:not(.occupied)')) {
+for (i = 0; i < 48; i++) {
+    
+    if (document.querySelector(`.seat[data-sn="${i}"]`).classList.contains('occupied')) {
 
-        e.target.classList.toggle('selected')
+        seatsArr[i] = 2
+    } else {
+
+        seatsArr[i] = 0
     }
+}
+
+seats.addEventListener("click", (e) => {
+  if (e.target.matches(".seat:not(.occupied)")) {
+
+    e.target.classList.toggle("selected")
+
+    if (e.target.classList.contains('selected')) {
+
+        seatsArr[e.target.dataset.sn] = 1
+        selectedSeatsCount.textContent++
+    } else {
+
+        seatsArr[e.target.dataset.sn] = 0
+        selectedSeatsCount.textContent--
+    }
+  }
 })
