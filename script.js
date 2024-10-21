78e90a43ca05b0354291e799aaa166b4061d9acb
Kareemayman <Kareem.Mohamed.ayman@gmail.com>
2024-10-21 19:57:18 +0300

    Saving & Loading Data From Browser Storage

--- script.js
+++ script.js
@@ -3,22 +3,46 @@ const selectedSeatsCount = document.querySelector(".selection .num-of-seats")
 const selectedMovie = document.querySelector('.select-movie select')
 const price = document.querySelector('.selection .price')
 
-let moviePrice = parseInt(selectedMovie.value)
+selectedMovie.value = localStorage.getItem('selectedMovie') || selectedMovie.value
 
-let seatsArr = [] // 0 => empty, 1 => selected, 2 => occupied
+let moviePrice = parseInt(localStorage.getItem('selectedMovie')) || parseInt(selectedMovie.value)
 
-for (i = 0; i < 48; i++) {
-    
-    if (document.querySelector(`.seat[data-sn="${i}"]`).classList.contains('occupied')) {
+price.textContent = parseInt(localStorage.getItem('price')) || 0
 
-        seatsArr[i] = 2
-    } else {
+selectedSeatsCount.textContent = parseInt(localStorage.getItem('seatsCount')) || 0
 
-        seatsArr[i] = 0
+let seatsArr = JSON.parse(localStorage.getItem('seatsArr')) || [] // 0 => empty, 1 => selected, 2 => occupied
+
+if (!JSON.parse(localStorage.getItem('seatsArr'))) {
+
+    for (i = 0; i < 48; i++) {
+        
+        if (document.querySelector(`.seat[data-sn="${i}"]`).classList.contains('occupied')) {
+        
+            seatsArr[i] = 2
+        } else {
+        
+            seatsArr[i] = 0
+        }
+    }
+} else {
+
+    for (i = 0; i < 48; i++) {
+
+        if (seatsArr[i] === 0) {
+            
+            document.querySelector(`.seat[data-sn="${i}"`).className = "seat"
+        } else if (seatsArr[i] === 1) {
+
+            document.querySelector(`.seat[data-sn="${i}"`).className = "seat selected"
+        } else {
+
+            document.querySelector(`.seat[data-sn="${i}"`).className = "seat occupied"
+        }
     }
 }
 
-// When Clicking On An Empty Seat
+// When Clicking On A Seat
 seats.addEventListener("click", (e) => {
 
   if (e.target.matches(".seat:not(.occupied)")) {
@@ -35,7 +59,11 @@ seats.addEventListener("click", (e) => {
         selectedSeatsCount.textContent--
     }
 
+    localStorage.setItem('seatsCount', selectedSeatsCount.textContent)
+
     calculatePrice()
+
+    localStorage.setItem('seatsArr', JSON.stringify(seatsArr))
   }
 })
 
@@ -45,6 +73,8 @@ selectedMovie.addEventListener('change', e => {
     moviePrice = selectedMovie.value
 
     calculatePrice()
+
+    localStorage.setItem('selectedMovie', moviePrice)
 })
 
 // Calculate Total Price For Selected Seats & Movie
@@ -53,4 +83,6 @@ function calculatePrice() {
     let newPrice = moviePrice * selectedSeatsCount.textContent
 
     price.textContent = newPrice
+
+    localStorage.setItem('price', newPrice)
 }
